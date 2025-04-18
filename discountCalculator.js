11125b4c7a7374978db05b6b93931a7fe1192e34
Michael McCormack <mcctwin@gmail.com>
2025-04-17 19:11:05 -0500

    2 tests passed

--- discountCalculator.js
+++ discountCalculator.js
@@ -1,17 +1,18 @@
+debugger
 function calculateDiscountedPrice(quantity, pricePerItem) {
     let totalPrice = 0;
-    debugger
-    for (let i = 1; i < quantity; i++) {
+debugger
+    for (let i = 1; i <= quantity; i++) {
         totalPrice += pricePerItem;
     }
-
+debugger
     if (quantity >= 10) {
         totalPrice *= 0.9;
     }
-
+debugger
     return totalPrice;
 }
 
-
+console.log(calculateDiscountedPrice(5, 20))
 
 module.exports = calculateDiscountedPrice;
